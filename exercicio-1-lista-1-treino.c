5b3298b86b23208dc74f55dfca2c80c1750ca326
Pedro Arthur Alves de Oliveira <69775937+Lahat-Herev@users.noreply.github.com>
2022-05-17 22:22:29 -0300

    Exercise 1 list 1 training

---
+++ exercicio-1-lista-1-treino.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, x;
+	printf("Digite o primeiro numero: \n");
+	scanf("%d", &a);
+	printf("Digite o segundo numero: \n");
+	scanf("%d", &b);
+	x = a + b;
+	printf("O resultado de a + b e: %d", x);
+	return 0;
+}
