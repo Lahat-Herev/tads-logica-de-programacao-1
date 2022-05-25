ffa1174a5a27bd97fdf9e029e983f3cc2bce0838
Pedro Arthur Alves de Oliveira <69775937+Lahat-Herev@users.noreply.github.com>
2022-05-25 19:20:25 -0300

    Exercise 3 list 1.1

---
+++ exercicio-3-lista-1-treino.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int valorA, valorB, soma;
+	printf("\n Entre com o valor A: ");
+	scanf("%d", &valorA);
+	printf("\n Entre com o valor B: ");
+	scanf("%d", &valorB);
+	soma = valorA + valorB;
+	printf("SOMA = %d \n", soma);
+	return 0;
+}
