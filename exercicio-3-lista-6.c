dc1c342fab82a9f6223235ff742a03a3902dd7d8
Lahat-Herev <pedroarthuralvesdeoliveira1@gmail.com>
2022-06-18 17:01:49 -0300

    Finally: exercise 3 in a normal way

--- exercicio-3-lista-6.c
+++ exercicio-3-lista-6.c
@@ -1,13 +1,14 @@
+#include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 int main(){
     int contador = 0;
-    int numeroMaximoPessoas = 2;
+    int numeroMaximoPessoas = 3;
     int idade;
     int maiorIdade = 0; 
-    int menorIdade = 0; 
+    int menorIdade = idade; 
     int quantidadeHomens = 0; 
     int quantidadeMulheres = 0;
     int somaIdadeMulheres = 0;
@@ -31,12 +32,15 @@ int main(){
             quantidadeHomens++;
             break;
         }
-        maiorIdade += (idade < maiorIdade ? maiorIdade : idade);
-        menorIdade += (idade < menorIdade ? idade: menorIdade); 
+
+        menorIdade = (idade < menorIdade ? idade : menorIdade);
+        maiorIdade = (idade < maiorIdade ? maiorIdade : idade);
         
         contador++;
     } while (contador <= numeroMaximoPessoas);
+    mediaIdadeMulheres = somaIdadeMulheres / quantidadeMulheres;
     printf("A quantidade de homens e: %d \n", quantidadeHomens);
     printf("A quantidade de mulheres e: %d \n", quantidadeMulheres);
-    // printf("A maior idade e: \nA menor idade e: \n", &maiorIdade, &menorIdade);
+    printf("A maior idade e: %d \nA menor idade e: %d \n", maiorIdade, menorIdade);
+    printf("A media de idade das mulheres e: %.2f", mediaIdadeMulheres);
 }
