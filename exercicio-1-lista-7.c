be327b7840c4c709e552c22885cfc5ed1846b4c0
Pedro Arthur Alves de Oliveira <69775937+Lahat-Herev@users.noreply.github.com>
2022-06-22 22:04:33 -0300

    Exercise 1 of list 7

---
+++ exercicio-1-lista-7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+	int fimCodigoASCII = 126;
+	for (int inicioCodigoASCII = 32; inicioCodigoASCII <= fimCodigoASCII; inicioCodigoASCII++)
+	{
+		printf(" %c", inicioCodigoASCII);
+	}
+}
