cf5552e2268b3a3b455111867bb9bc5870a84d66
Pedro Arthur Alves de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2022-07-01 18:06:12 -0300

    Tratando o erro nan

--- exercicio-5-lista-7.c
+++ exercicio-5-lista-7.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int valorInicial;
-    int valorFinal;
+    double mediaInteirosPositivos;
+    double mediaPares;
+    double mediaImpares;
+    double mediaImparesDivisiveisPor3e4;
     int quantidadeInteirosPositivos = 0;
     int quantidadePares = 0;
     int quantidadeImpares = 0;
@@ -12,6 +15,8 @@ int main()
     int somaInteirosPositivos = 0;
     int somaImpares = 0;
     int somaQuantidadeImparesDivisiveisPelosNumeros = 0;
+    int valorInicial;
+    int valorFinal;
 
     printf("Digite o valor inicial: ");
     scanf("%d", &valorInicial);
@@ -38,14 +43,35 @@ int main()
             somaImpares += contador;
         }
 
-        if ((contador % 3 == 0) && (contador % 4 == 0) && (contador != 0))
+        if ((contador % 3 == 0) && (contador % 4 == 0) && (contador % 2 != 0))
         {
             quantidadeImparesDivisiveisPelosNumeros++;
             somaQuantidadeImparesDivisiveisPelosNumeros += contador;
         }
     }
-    printf("\nA quantidade de numeros inteiros e positivos e %d e a media dos mesmos e: %2.f", quantidadeInteirosPositivos, (double) somaInteirosPositivos/quantidadeInteirosPositivos);
-    printf("\nA quantidade de numeros pares e %d e a media dos mesmos e: %.2f", quantidadePares, (double) somaPares/quantidadePares);
-    printf("\nA quantidade de numeros impares e %d e a media dos mesmos e: %.2f", quantidadeImpares, (double) somaImpares/quantidadeImpares);
-    printf("\nA quantidade de numeros impares divisiveis por [3] e [4] e: %d, e a media dos mesmos e: %.2f", quantidadeImparesDivisiveisPelosNumeros, (double) somaQuantidadeImparesDivisiveisPelosNumeros/quantidadeImparesDivisiveisPelosNumeros);
+    
+    mediaInteirosPositivos = (double) somaInteirosPositivos/quantidadeInteirosPositivos;
+    mediaPares = (double) somaPares/quantidadePares;
+    mediaImpares = (double) somaImpares/quantidadeImpares;
+    mediaImparesDivisiveisPor3e4 = (double) somaQuantidadeImparesDivisiveisPelosNumeros/quantidadeImparesDivisiveisPelosNumeros;
+
+    if (mediaInteirosPositivos > 0.0) 
+        printf("\nA quantidade de numeros inteiros e positivos e %d e a media dos mesmos e: %2.f", quantidadeInteirosPositivos, mediaInteirosPositivos);
+    if (mediaInteirosPositivos <= 0.0)
+        printf("Nao existe(m) inteiro(s) positivo(s).");
+
+    if (mediaPares > 0.0) 
+        printf("\nA quantidade de numeros pares e %d e a media dos mesmos e: %.2f", quantidadePares, mediaPares);
+    if (mediaPares <= 0.0)
+        printf("Nao existe(m) numero(s) par(es).");
+    
+    if (mediaImpares > 0.0) 
+        printf("\nA quantidade de numeros impares e %d e a media dos mesmos e: %.2f", quantidadeImpares, mediaImpares);
+    if (mediaImpares <= 0.0)
+        printf("Nao existe(m) numero(s) impar(es).");
+    
+    if (mediaImparesDivisiveisPor3e4 == mediaImparesDivisiveisPor3e4) 
+        printf("\nA quantidade de numeros impares divisiveis por [3] e [4] e: %d, e a media dos mesmos e: %.2f", quantidadeImparesDivisiveisPelosNumeros, mediaImparesDivisiveisPor3e4);
+    if (mediaImparesDivisiveisPor3e4 != mediaImparesDivisiveisPor3e4)
+        printf("\nNao existe(m) numero(s) impar(es) divisiveis(el) por 3 e por 4.");
 }
